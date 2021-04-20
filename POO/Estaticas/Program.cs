ebb306c912bb6cafb8d201154dbcba2328a3c65f
eduandrade <eduandrade1979@outlook.com>
2021-04-20 01:50:57 -0300

    Atualização 20.04.2021

---
+++ POO/Estaticas/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estaticas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Matematica m = new Matematica();  => // NÃO É POSSIVEL CRIAR UM OBJETO 
+                                                    // A PARTIR DE UMA CLASSE ESTÁTICA.
+
+            Matematica.taxa = 10;  // Para acesar os membros, apenas referencia-se o nome da classe.
+                                   // Como a classe está sendo referenciada, uma vez que TAXA recebesse
+                                   // outro valor, qualquer lugar da aplicação que referenciasse a classe
+                                   // teria seu valor alterado imediatamente.
+                                   // STATIC => Costuma ser utilizado para configurações que sejam globais
+
+            int valor1 = Matematica.adicionar(200);
+            int valor2 = Matematica.diminuir(200);
+
+            Console.WriteLine("Valor 1 = " + valor1);
+            Console.WriteLine("Valor 2 = " + valor2);
+            
+            Console.ReadKey();
+        }
+    }
+}
